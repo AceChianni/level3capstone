5873325c83d9b1211bee8d2c993d9f855d931fe0
Your NameAce <chantricelacabe@gmail.com>
2024-11-25 13:21:16 -0600

    edit navbar padding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,32 +3,22 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-[#ae9bff] fixed top-0 w-full z-50 flex justify-center p-4 shadow-md">
-      <ul className="flex space-x-6 text-white">
+    <nav className="navbar">
+      <ul>
         <li>
-          <Link href="/" className="hover:underline">
-            Home
-          </Link>
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/about" className="hover:underline">
-            About
-          </Link>
+          <Link href="/about">About</Link>
         </li>
         <li>
-          <Link href="/quiz" className="hover:underline">
-            Quiz
-          </Link>
+          <Link href="/quiz">Quiz</Link>
         </li>
         <li>
-          <Link href="/contact" className="hover:underline">
-            Contact Us
-          </Link>
+          <Link href="/contact">Contact Us</Link>
         </li>
         <li>
-          <Link href="/gallery" className="hover:underline">
-            Gallery
-          </Link>
+          <Link href="/gallery">Gallery</Link>
         </li>
       </ul>
     </nav>
