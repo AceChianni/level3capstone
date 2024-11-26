480bf65fcd38b819c2f0d293072fe46d73e16fd1
Your NameAce <chantricelacabe@gmail.com>
2024-11-25 21:26:03 -0600

    edit unescaped entities on about/page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,14 +7,14 @@ export default function AboutPage() {
       <h2>About Me</h2>
       <section className="about-me">
         <p>
-          Hi there! I'm Ace Chianní (she/her), a New Orleans-based creative with
-          a love for art, anime, gaming, and all things nerdy. I’m driven by a
-          passion for crafting meaningful, engaging experiences that merge
-          creativity with technology. Whether it’s through design, storytelling,
-          or exploration, my goal is to create impactful, immersive works that
-          inspire and connect. As I transition into the tech space, I’m excited
-          to bring fresh, innovative ideas to life and share my journey of
-          discovery and creation with the world.
+          Hi there! I&apos;m Ace Chianní (she/her), a New Orleans-based creative
+          with a love for art, anime, gaming, and all things nerdy. I&apos;m
+          driven by a passion for crafting meaningful, engaging experiences that
+          merge creativity with technology. Whether it&apos;s through design,
+          storytelling, or exploration, my goal is to create impactful,
+          immersive works that inspire and connect. As I transition into the
+          tech space, I&apos;m excited to bring fresh, innovative ideas to life
+          and share my journey of discovery and creation with the world.
         </p>
       </section>
       <hr className="section-divider" />
@@ -88,11 +88,11 @@ export default function AboutPage() {
       <section className="why">
         <p>
           <strong>Anniime Recommendations</strong> represents my passion for
-          creativity and technology. It’s a space where I can showcase my design
-          and development skills while sharing my love for anime with the world.
-          This project is a step toward my dream of becoming a freelance artist
-          and entrepreneur, traveling the world, and inspiring others through my
-          work.
+          creativity and technology. It&apos;s a space where I can showcase my
+          design and development skills while sharing my love for anime with the
+          world. This project is a step toward my dream of becoming a freelance
+          artist and entrepreneur, traveling the world, and inspiring others
+          through my work.
         </p>
       </section>
     </div>
